2131f2349161eb717996d8807dc351554244062c
Ross Yeager <ryeager12@gmail.com>
2025-10-11 16:02:11 -0700

    wip

--- src/ry_pg_utils/ipc/channels.py
+++ src/ry_pg_utils/ipc/channels.py
@@ -1,12 +1,12 @@
 from ry_redis_bus.channels import Channel
 
 from ry_pg_utils.pb_types.database_pb2 import DatabaseConfigPb  # pylint: disable=no-name-in-module
-from ry_pg_utils.pb_types.database_pb2 import (
+from ry_pg_utils.pb_types.database_pb2 import (  # pylint: disable=no-name-in-module
     DatabaseNotificationPb,
-)  # pylint: disable=no-name-in-module
-from ry_pg_utils.pb_types.database_pb2 import (
+)
+from ry_pg_utils.pb_types.database_pb2 import (  # pylint: disable=no-name-in-module
     DatabaseSettingsPb,
-)  # pylint: disable=no-name-in-module
+)
 
 # Channels
 DATABASE_CHANNEL = Channel("DATABASE_CHANNEL", DatabaseConfigPb)
