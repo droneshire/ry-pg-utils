5fbe99e29734ba6df80b1168b84361957e282437
Ross Yeager <ryeager12@gmail.com>
2025-10-18 14:06:59 -0700

    adding missing arg

--- src/ry_pg_utils/parse_args.py
+++ src/ry_pg_utils/parse_args.py
@@ -12,3 +12,9 @@ def add_postgres_db_args(parser: argparse.ArgumentParser) -> None:
     postgres_parser.add_argument("--postgres-user", default=config.postgres_user)
     postgres_parser.add_argument("--postgres-password", default=config.postgres_password)
     postgres_parser.add_argument("--do-publish-db", action="store_true", default=False)
+    postgres_parser.add_argument(
+        "--use-local-db-only",
+        action="store_true",
+        default=config.use_local_db_only,
+        help="Use only local database connections (no Redis-based config updates)",
+    )
